93b45571f2f11d3f98b190027d3de9b0f8b0cdfc
Nick Tang <nicholastang@email.arizona.edu>
2021-10-12 11:07:10 -0700

    "Initial Commit"

---
+++ app.R
@@ -0,0 +1,173 @@
+#
+#Author: Nick Tang
+#ISTA320
+#Final Project
+#This script is to create a shiny app that displays visualizations
+#from a data source on kaggle
+#
+
+library(lubridate)
+library(usmap)
+library(shiny)
+library(tidyverse)
+mental_health_survey_data <- read_csv("data/survey.csv")
+
+#change Gender col to lower for pre-processing
+mental_health_survey_data$Gender <- tolower(mental_health_survey_data$Gender)
+male <- c("male","m","male-ish", "cis male","mail","malr", "mal","make","msle","cis man")
+female <- c("female","trans-female","cis female","f","female (trans)","female (cis)","woman")
+other_gender <- c("queer/she/they","non-binary","nah","all","enby","fluid","genderqueer","androgyne",
+                  "agender","male leaning androgynous","queer","a little about you","p")
+
+min(mental_health_survey_data[,'Age']) 
+max(mental_health_survey_data[,'Age'])
+#min and max ages are unrealistic, will filter out
+
+
+#tidying data to make sure the person works for tech company
+#and to select the desired columns for viz
+mental_health_survey_data_tidy <- mental_health_survey_data %>% 
+    filter(tech_company=='Yes',!is.na(work_interfere),!is.na(Age), Age>11, Age<100,!is.na(state)) %>% 
+    select(Age,Gender,state,family_history:tech_company)
+
+#changing Gender col to male, female, or other
+mental_health_survey_data_tidy$Gender <-  ifelse(mental_health_survey_data_tidy$Gender %in% male, 'male', mental_health_survey_data_tidy$Gender) 
+mental_health_survey_data_tidy$Gender <-  ifelse(mental_health_survey_data_tidy$Gender %in% female, 'female', mental_health_survey_data_tidy$Gender)
+mental_health_survey_data_tidy$Gender <-  ifelse(mental_health_survey_data_tidy$Gender %in% other_gender, 'other', mental_health_survey_data_tidy$Gender)
+
+#make new col for age groups
+mental_health_survey_data_tidy <- mental_health_survey_data_tidy %>% 
+    mutate(age_group = case_when(Age >= 18  & Age < 25 ~ '18-24',
+                                 Age >= 25  & Age < 31 ~ '25-30',
+                                 Age >= 31  & Age <36  ~ '31-35',
+                                 Age >= 36  & Age <41  ~ '36-40',
+                                 Age >= 41  & Age <46  ~ '41-45',
+                                 Age >= 46  & Age <51  ~ '46-50',
+                                 Age >= 51  & Age <56  ~ '51-55',
+                                 Age >= 56  & Age <61  ~ '56-60',
+                                 Age >= 61  & Age <66  ~ '61-65',
+    ))
+
+mental_health_survey_data_tidy$work_interfere <- factor(mental_health_survey_data_tidy$work_interfere,
+                                                        levels=c("Never", "Rarely", "Sometimes","Often"))
+
+age_options <- mental_health_survey_data_tidy %>%
+    distinct(age_group) %>%
+    arrange(age_group)
+
+gender_options <- mental_health_survey_data_tidy %>%
+    distinct(Gender) %>%
+    arrange(Gender)
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Nick Tang ISTA320 Final Project"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        fluid=TRUE,
+        sidebarPanel(
+            
+            tags$div(class="header", checked=NA,
+                     p("These visualizations analyze data from Kaggle about worker's mental health in the tech industry"),
+                     a(href="https://www.kaggle.com/osmi/mental-health-in-tech-survey
+ ", "Data Source"),
+                     hr(),
+                     p("This data was collected as a survey and needed a lot of\n
+                        preprocessing to be able to make visualizations.\n
+                        Some of the most used columns in this data set include:\n
+                        ",br(),
+                       strong("Age"),br(),
+                       strong("Gender"),br(),
+                       strong("State"),br(),
+                       strong("family_history"),br(),
+                       strong("no_employees"))
+                     
+            )
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Ages vs Mental Health", 
+                                 selectInput("age_to_highlight",
+                                             "Select Age Group to Plot:",
+                                             choices = age_options),
+                                 plotOutput("age_plot")),
+                        tabPanel("Genders vs Mental Health",
+                                 selectInput("gender_to_highlight",
+                                             "Select Gender to Plot:",
+                                             choices = gender_options),
+                                 plotOutput("gender_plot")),
+                        tabPanel("Tech Age Density",
+                                 plotOutput("usa_plot")),
+                        tabPanel("Company Size",
+                                 plotOutput("company_size_plot")),
+                        tabPanel("Family History",
+                                 plotOutput("family_history_plot")))
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$age_plot <- renderPlot({
+        mental_health_survey_data_tidy %>%
+            filter(age_group == input$age_to_highlight) %>% 
+            ggplot(aes(x=work_interfere,y=age_group,col=work_interfere)) +
+            geom_point()+
+            geom_jitter(width = .3, height = .4, size = 3,alpha = .8)+
+            ggtitle("How Tech Jobs Affect Mental Health (Age)") +
+            xlab("How Much Work Interferes With Mental Health")+
+            ylab("Age Group")+
+            scale_color_manual(values = c("red", "seagreen", "dodgerblue2","orange"))
+    })
+    
+    output$gender_plot <- renderPlot({
+        mental_health_survey_data_tidy %>%
+            filter(Gender == input$gender_to_highlight) %>% 
+            ggplot(aes(x=work_interfere,y=Gender,col=work_interfere)) +
+            geom_point()+
+            geom_jitter(width = .3, height = .4, size = 3, alpha = .8)+
+            ggtitle("How Tech Jobs Affect Mental Health (Gender)") +
+            xlab("How Much Work Interferes With Mental Health")+
+            ylab("Gender")+
+            scale_color_manual(values = c("red", "black", "dodgerblue2","orange"))
+    })
+    
+    output$usa_plot <- renderPlot({
+        mental_health_survey_data_tidy %>% 
+            plot_usmap(data = ., 
+                       values = "Age") +
+            theme(legend.position = "right") +
+            scale_fill_continuous(name = "Ages",
+                                  low = "lightblue", 
+                                  high = "darkorchid") +
+            ggtitle("Ages of Tech Workers Across the United States")
+    })
+    
+    output$company_size_plot <- renderPlot({
+        mental_health_survey_data_tidy %>% 
+            ggplot(aes(x=work_interfere,fill=no_employees))+
+            geom_bar()+
+            ggtitle("Number of Employees and Mental Helath") +
+            xlab("How Much Work Interferes With Mental Health") +
+            ylab("Count")
+        
+    })
+    
+    output$family_history_plot <- renderPlot({
+        
+        mental_health_survey_data_tidy %>% 
+            filter(work_interfere == "Sometimes" | work_interfere == "Often") %>% 
+            ggplot(aes(x=work_interfere,fill=family_history))+
+            geom_bar() +
+            ggtitle("Worker's mental health and Their Family History") +
+            xlab("How Much Work Interferes With Mental Health") +
+            ylab("Count")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
